646a0f637fa572040b0aa4b6e6b396b562a57d80
WOoOinux <contact@woinux.fr>
2015-07-13 17:40:13 +0200

    Create main function

---
+++ som.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*-coding:Utf-8 -*-
+
+nbrAccounts = 0
+while nbrAccounts <= 0 :
+    nbrAccounts = input("Combien de comptes souhaitez-vous ajouter (> à 0) ? ")
+    nbrAccounts = int(nbrAccounts)
+
+# On lance la création des comptes
+
+# On lance la création de la BDD
+
+# On lance la création du virtualhost
+
+# On envoi les informations des comptes par email
